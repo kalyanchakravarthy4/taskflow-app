aad1baaa72671062a9bb2417c4d04d6db5baaa0d
Kancharapu Kalyan Chakravarthy <kalyanchakravarth4@gmail.com>
2026-04-25 20:38:34 +0530

    Update axios js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://taskflow-app-0tt.onrender.com/api", // 🔥 your backend URL
+  baseURL: "https://taskflow-app-0tt.onrender.com/api"
 });
 
-// OPTIONAL: attach token automatically
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
